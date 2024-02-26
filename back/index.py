64590c3423ffdb758a86ec4e740440793eee3ab3
nurbergenovv <ibrahimffyt@gmail.com>
2024-02-27 01:45:22 +0600

    asd

--- back/index.py
+++ back/index.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify
 import usb.core
 import os
+import cups
 
 app = Flask(__name__)
+
+def get_printer_list():
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+    return printers
+
 
 def get_usb_devices():
     devices = usb.core.find(find_all=True)
@@ -13,7 +20,10 @@ def get_usb_devices():
             "Files:": get_flash_drive_path(device.idVendor),
         }
         device_list.append(device_info)
-    return device_list
+    if device_list:
+        return jsonify({"status":"success","message":"Flash list successfully finded", "USBList":files})
+    else:
+        return jsonify({"status":"error","message":"Flash not find"})
 
 def get_flash_drive_path(idVendor):
     vendor_id = hex(idVendor)
@@ -25,7 +35,7 @@ def get_flash_drive_path(idVendor):
                 files.append(i)
         return files
     else:
-        return "Flash drive path not found"
+        return []
 
 @app.route("/")
 def index():
@@ -35,6 +45,10 @@ def index():
 def devices():
     devices = get_usb_devices()
     return jsonify(devices)
+
+@app.route("/printers")
+def printers():
+    return jsonify(get_printer_list())
 
 if __name__ == "__main__":
     app.run(debug=True)
