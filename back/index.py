ffcf36250cf347ab08dccbca41ac2209dfe3444c
nurbergenovv <ibrahimffyt@gmail.com>
2024-02-26 23:38:19 +0600

    qwerqwe

--- back/index.py
+++ back/index.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import usb.core
 import os
 
 app = Flask(__name__)
-
 
 def get_usb_devices():
     devices = usb.core.find(find_all=True)
     device_list = []
     for device in devices:
         device_info = {
-            "Device vendor:": hex(device.idVendor),
-            "Path:": get_flash_drive_path(device.idVendor)
+            "Device name:": os.listdir(f"/Volumes")[1],
+            "Files:": get_flash_drive_path(device.idVendor),
         }
         device_list.append(device_info)
     return device_list
@@ -19,14 +18,14 @@ def get_usb_devices():
 def get_flash_drive_path(idVendor):
     vendor_id = hex(idVendor)
 
-    usb_mount_path = "/System/"
-
-    for root, dirs, files in os.walk(usb_mount_path):
-        for directory in dirs:
-            if directory.startswith(vendor_id):
-                return os.path.join(root, directory)
-
-    return "Flash drive path not found"
+    if os.listdir(f"/System"):
+        files = []
+        for i in os.listdir(f"/Volumes/{os.listdir(f"/Volumes")[1]}"):
+            if i.endswith(".docx"):
+                files.append(i)
+        return files
+    else:
+        return "Flash drive path not found"
 
 @app.route("/")
 def index():
@@ -35,7 +34,7 @@ def index():
 @app.route("/devices")
 def devices():
     devices = get_usb_devices()
-    return str(devices)
+    return jsonify(devices)
 
 if __name__ == "__main__":
     app.run(debug=True)
